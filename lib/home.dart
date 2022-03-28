19181ec9af9f9ae9eff5a56d6acf97ce06bb67aa
Aymie Chalouhy <aymie.walid.chalouhy@gmail.com>
2022-03-28 13:30:38 +0300

    play sound

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -17,34 +18,38 @@ class _HomePageState extends State<HomePage> {
           width: size.width,
           decoration: const BoxDecoration(
               gradient: LinearGradient(
-              colors: [Colors.blue, Colors.lightBlue, Colors.pink])),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center, 
-            children:  [
-            const Text("Let's Sing", style: TextStyle(fontWeight: FontWeight.bold, fontSize:40 ),
+                  colors: [Colors.blue, Colors.lightBlue, Colors.pink])),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+            const Text(
+              "Let's Sing",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
             ),
-            
-          const   SizedBox(
-               height: 20,
-             ),
-           const    Text("“Words make you think. \n Music makes you feel.”",
-              style: TextStyle(fontWeight: FontWeight.bold)
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top:30),
-                child: SizedBox(
-                 height: size.height /2.8,
-                 child: const Image(image: AssetImage("assets/sing.png"))),
-              ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text("“Words make you think. \n Music makes you feel.”",
+                style: TextStyle(fontWeight: FontWeight.bold)),
+            Padding(
+              padding: const EdgeInsets.only(top: 30),
+              child: SizedBox(
+                  height: size.height / 2.8,
+                  child: const Image(image: AssetImage("assets/sing.png"))),
+            ),
             ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(30.0)
-                ),
-                primary: Colors.white, elevation: 5),
-               child: const Text(
-              "Sing", style:TextStyle(fontSize: 20,color: Color.fromARGB(255, 0, 0, 0), )))
+                onPressed: () {
+                  final player = AudioCache();
+                  player.play("song.mp3");
+                },
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(30.0)),
+                    primary: Colors.white,
+                    elevation: 5),
+                child: const Text("Sing",
+                    style: TextStyle(
+                      fontSize: 20,
+                      color: Color.fromARGB(255, 0, 0, 0),
+                    )))
           ]),
         ),
       ),
